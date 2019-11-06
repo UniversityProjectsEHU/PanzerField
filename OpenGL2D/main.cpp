4aaa375b7367a299e9305ea4c7794bbd4d4fecd6
Sergio Hernando Gaston <sergiohg10@hotmail.es>
2019-11-06 18:48:19 +0400

    Fixed the initial commit

--- OpenGL2D/main.cpp
+++ OpenGL2D/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
 
 
 	//test objects
-	/*Sprite *pSprite1= new Sprite();
+	Sprite *pSprite1= new Sprite();
 	pSprite1->setColor(255, 0, 0);
 	pSprite1->setPosition(0.25, 0.25);
 	pSprite1->setRotation(0.0);
@@ -29,41 +29,6 @@ int main(int argc, char** argv)
 	pSprite2->setPosition(0.5, 0.5);
 	pSprite2->setRotation(45.0);
 	pSprite2->setSize(0.2);
-	pSprite2->setDepth(1.3);
-	renderer.addObject(pSprite2);*/
-
-	//CREEPER
-
-	//OJOH
-	Sprite *pSprite1 = new Sprite();
-	pSprite1->setColor(0, 0.5, 0);
-	pSprite1->setPosition(-0.25, 0.4);
-	pSprite1->setRotation(0);
-	pSprite1->setSize(0.1);
-	pSprite1->setDepth(1.3);
-	renderer.addObject(pSprite1);
-	//OJOH2
-	Sprite *pSprite3 = new Sprite();
-	pSprite3->setColor(0, 0.5, 0);
-	pSprite3->setPosition(0.25, 0.4);
-	pSprite3->setRotation(0);
-	pSprite3->setSize(0.1);
-	pSprite3->setDepth(1.3);
-	renderer.addObject(pSprite3);
-	//BOCA
-	Sprite *pSprite4 = new Sprite();
-	pSprite4->setColor(150, 0, 0);
-	pSprite4->setPosition(0, 0.8);
-	pSprite4->setRotation(0);
-	pSprite4->setSize(0.4);
-	pSprite4->setDepth(1.3);
-	renderer.addObject(pSprite4);
-	//CABESA
-	Sprite *pSprite2 = new Sprite();
-	pSprite2->setColor(0, 255, 0);
-	pSprite2->setPosition(0, 0.2);
-	pSprite2->setRotation(0);
-	pSprite2->setSize(0.5);
 	pSprite2->setDepth(1.3);
 	renderer.addObject(pSprite2);
 
