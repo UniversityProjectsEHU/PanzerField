e53dcd42a0bbc293b8ba74e59559524b619dbdb7
Nagore Miguel <nagomiguel@hotmail.com>
2019-11-13 16:26:48 +0100

    Map has been

--- OpenGL2D/Map.h
+++ OpenGL2D/Map.h
@@ -5,15 +5,34 @@
 #include "Drawable.h"
 
 using namespace std;
-class Map
+class Map: public Drawable
 {
 	string nameFile;
 	char line;
 	vector <char> m_map;
 	int m_height;
 	int m_width;
+
+	float m_r, m_g, m_b;
+
+	double m_x = 0.0, m_y = 0.0; //position around which the quad is drawn
+
+	double m_depth = 1.0; // the renderer draws depths in range [1.0,20.0]
+
+	double m_size = 1.0; //size of the quad
+
+	double m_angle = 0.0; //rotation angle
+
 public:
 	void createMap(string nameFile);
+	void draw();
+	void createWallSprite(double x, double y);
+	void createFieldSprite(double x, double y);
+	void setColor(float r, float g, float b);
+	void setPosition(double x, double y);
+	void setDepth(double depth);
+	void setRotation(double angle);
+	void setSize(double size);
 	Map(string namefile);
 	~Map();
 };
--- OpenGL2D/pruebas.cpp
+++ OpenGL2D/pruebas.cpp
@@ -3,9 +3,32 @@
 #include "iostream"
 #include <chrono>
 #include <thread>
+#include "InputHandler.h"
+#include "Renderer.h"
+#include "../3rd-party/freeglut3/include/GL/freeglut.h"
 
-int main()
+int main(int argc, char** argv)
 {
+	Renderer renderer;
+	InputHandler inputHandler(renderer);
+
+	renderer.initialize(argc, argv);
+	inputHandler.initialize();
 	Map map("Map1.txt");
-	return 0;
+	while (1)
+			{
+				//UPDATE////////////////////
+				////////////////////////////
+				//process queued events
+				glutMainLoopEvent();
+		
+		
+				//RENDER////////////////////
+				////////////////////////////
+				glutPostRedisplay();
+				glutSwapBuffers();
+			}
+		   
+			return 0;
 }
+
