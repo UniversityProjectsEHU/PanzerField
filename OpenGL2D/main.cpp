44cb73fc6661c4998eef72ed5ded9658f4de30c6
docencia <docencia@U030500>
2019-11-20 17:58:59 +0100

    Changes in main, add enemy

--- OpenGL2D/main.cpp
+++ OpenGL2D/main.cpp
@@ -37,6 +37,19 @@ int main(int argc, char** argv)
 	tank->setVel(0,0);
 
 	renderer.addObject(tank);
+	//We create a enemy tank
+	TankEnemy *tank2 = new TankEnemy();
+
+	tank2->setColor(0, 0, 255);
+	tank2->setPosition(0.5, 0);
+	tank2->setSize(0.02);
+	tank2->setRotation(0);
+	tank2->setDepth(1.5);
+
+	tank2->setName("tank2");
+	tank2->setVel(0, 0);
+
+	renderer.addObject(tank2);
 	char character;
 	string cad;
 	std::cout << "Please press a number from 1 to 5";
