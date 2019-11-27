c81cf8a624bf8cf7dd433c8aa013ae52f1893849
docencia <docencia@U030500>
2019-11-27 17:06:24 +0100

    Tank 2 movement

--- OpenGL2D/InputHandler.cpp
+++ OpenGL2D/InputHandler.cpp
@@ -83,6 +83,56 @@ void InputHandler::processKeyboard(unsigned char key, int x, int y)
 			theTank->shoot();
 		}
 		break;
+	//tank2
+		//1
+	case 49:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Tank* theTank = (Tank*)var;
+			double theRotation = theTank->getRotation();
+			theTank->setVelRotation(100);
+		}
+		break;
+		//5
+	case 53:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			theTank->setVel(0.5, 0.5);
+		}
+		break;
+		//2
+	case 50:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			theTank->setVel(-0.5, -0.5);
+		}
+		break;
+		//3
+	case 51:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			double theRotation = theTank->getRotation();
+			theTank->setVelRotation(-100);
+		}
+		break;
+	case 48:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Tank* theTank = (Tank*)var;
+			theTank->shoot();
+		}
+		break;
 	case 27: exit(0);
 	}
 }
@@ -129,6 +179,51 @@ void InputHandler::processKeyboard2(unsigned char key, int x, int y)
 		//d
 	case 100:
 		vectorObjects = m_renderer.getObjects("tank1");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			double theRotation = theTank->getRotation();
+			theTank->setVelRotation(0);
+
+		}
+		break;
+	
+		//tank2
+		//1
+	case 49:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			double theRotation = theTank->getRotation();
+			theTank->setVelRotation(0);
+		}
+		break;
+		//5
+	case 53:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			theTank->setVel(0, 0);
+		}
+		break;
+		//2
+	case 50:
+		vectorObjects = m_renderer.getObjects("tank2");
+		for each (Drawable* var in vectorObjects)
+		{
+			Sprite* theObject = (Sprite*)var;
+			Tank* theTank = (Tank*)theObject;
+			theTank->setVel(0, 0);
+		}
+		break;
+		//3
+	case 51:
+		vectorObjects = m_renderer.getObjects("tank2");
 		for each (Drawable* var in vectorObjects)
 		{
 			Sprite* theObject = (Sprite*)var;
